720c57f7375400c358a916c8a11335cac2cf49c2
Divya <divya.inapakurthi@gmail.com>
2025-01-25 12:23:40 -0600

    render

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// modules 
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("./config/mongoose");
@@ -9,7 +10,7 @@ require('dotenv').config()
 
 const port = process.env.PORT
 
-
+// for sass files
 app.use(sassMiddleware({
     src: "./assets/scss",
     dest: "./assets/css",
@@ -17,31 +18,46 @@ app.use(sassMiddleware({
     outputStyle: "expanded",
     prefix: "/css"
 }))
+// for comm with frontend
 app.use(cors());
 
 app.use(cors({
     origin: 'https://vrs-pickles-frontend.vercel.app/'
 }))
+
+
+// for reducing unnecessary html code 
 app.use(expressLayouts)
+
+// for form data 
 app.use(express.urlencoded({ extended: true }));
+
+// for accessing images or uploading
 app.use("/uploads", express.static(__dirname + "/uploads"));
-app.use("/css", express.static(__dirname + "/css"));
+
+
+// app.use("/css", express.static(__dirname + "/css"));
 
 // this helped to apply the continuous changes made to css
 app.use(express.static("assets"))
 
+// engine and layouts
 app.set("views", "views");
 app.set("view engine", "ejs");
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
 
+// server port 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 })
 
+// backend REST API's 
 
 app.use("/", require("./router"));
 
+
+// for frontend data
 app.get("/", (req, res) => {
     products.find({})
     .then(products => {
